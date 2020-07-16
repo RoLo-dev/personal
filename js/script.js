7f4b0f7c42b54481c5842c5cb844288845cfc690
Rolando Lopantzi <rlopantzi@gmail.com>
2020-07-16 08:44:00 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -120,26 +120,3 @@ AOS.init({
     offset: 300,
     duration: 850
 });
-
-
-// if(galleryImgs) {
-//   galleryImgs.forEach((image, index) => {
-//     image.onclick = function() {
-//       let getElementCSS = window.getComputedStyle(image);
-//       let getFullImgURL = getElementCSS.getPropertyValue("background-image");
-//       let getImgURL = getFullImgURL.split('/imgs/');
-//       let setNewImgURL = getImgURL[1].replace('")', '');
-
-//       previousImg = index + 1;
-//       let container = document.body;
-//       let newImgWindow = document.createElement('div');
-//       container.appendChild(newImgWindow);
-//       newImgWindow.setAttribute('class', 'img-window');
-//       newImgWindow.setAttribute('onclick', 'closeImg()');
-
-//       let newImg = document.createElement('img');
-//       newImgWindow.appendChild(newImg);
-//       newImg.setAttribute('src', `imgs/${setNewImgURL}`);
-//     }
-//   })
-// }
